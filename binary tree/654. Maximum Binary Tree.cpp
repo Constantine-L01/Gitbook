afc9f508d1945631f17cc629efa8ca1d3ecfef0f
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-02-07 17:37:00 +0800

    Create 654. Maximum Binary Tree.cpp

---
+++ binary tree/654. Maximum Binary Tree.cpp
@@ -0,0 +1,62 @@
+You are given an integer array nums with no duplicates. A maximum binary tree can be built recursively from nums using the following algorithm:
+
+    Create a root node whose value is the maximum value in nums.
+    Recursively build the left subtree on the subarray prefix to the left of the maximum value.
+    Recursively build the right subtree on the subarray suffix to the right of the maximum value.
+
+Return the maximum binary tree built from nums.
+
+Example 1:
+
+Input: nums = [3,2,1,6,0,5]
+Output: [6,3,5,null,2,0,null,null,1]
+Explanation: The recursive calls are as follow:
+- The largest value in [3,2,1,6,0,5] is 6. Left prefix is [3,2,1] and right suffix is [0,5].
+    - The largest value in [3,2,1] is 3. Left prefix is [] and right suffix is [2,1].
+        - Empty array, so no child.
+        - The largest value in [2,1] is 2. Left prefix is [] and right suffix is [1].
+            - Empty array, so no child.
+            - Only one element, so child is a node with value 1.
+    - The largest value in [0,5] is 5. Left prefix is [0] and right suffix is [].
+        - Only one element, so child is a node with value 0.
+        - Empty array, so no child.
+        
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* constructRoot(vector<int>& nums, int lo, int hi) {
+        if (lo > hi) {
+            return NULL;
+        }
+        
+        int max = -1;
+        int index = -1;
+        // find the largest element in current subarray
+        for(int i = lo; i <= hi; i++) {
+            if(max < nums[i]){
+                index = i;
+                max = nums[i];
+            }            
+        }
+        TreeNode* root = new TreeNode(max);
+        // must use lo and hi because need to consider the left subtree of current right subtree or right subtree of current left subtree. 
+        root->left = constructRoot(nums, lo, index - 1);
+        root->right = constructRoot(nums, index + 1, hi);
+        
+        return root;
+    }
+    
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return constructRoot(nums, 0, nums.size() - 1);
+    }
+};
