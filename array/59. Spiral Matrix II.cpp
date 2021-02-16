8768c9801b98f604884c35e1ec81f4c72610998d
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-02-16 18:50:50 +0800

    Update 59. Spiral Matrix II.cpp

--- array/59. Spiral Matrix II.cpp
+++ array/59. Spiral Matrix II.cpp
@@ -50,3 +50,52 @@ public:
         return res;
     }
 };
+
+// second solution without the need to check for square matrix that has odd number of columns and rows
+-> -> ->
+^  ->  |
+<- <-  | 
+    
+class Solution {
+public:
+    vector<vector<int>> generateMatrix(int n) {
+        int startX = 0;
+        int startY = 0;
+        vector<vector<int>> res(n, vector<int>(n, 0));
+        int count = 1;
+        int i = 0, j = 0;
+        
+        while(count <= n*n) {
+            cout << "Left to right" << endl;
+            // go until last column 
+            for(j = startY; j < n - startY ; j++) {
+                cout << "startX " << startY << " j " << j << endl;
+                res[startX][j] = count++;        
+            }
+
+            cout << "Top to bottom" << endl;
+            // start i at one row below the starting row and start j at last column, go until last row
+            for(i = startX + 1, j = n - startY - 1; i < n - startX; i++) {
+                cout << "i " << i << " j " << j << endl;
+                res[i][j] = count++;
+            }
+
+            cout << "Right to left" << endl;
+            // start i at last row and start j at one column in front of the last column, go until first column
+            for(i = n - startX - 1, j = n - startY - 2; j >= startY; j--) {
+                cout << "i " << i << " j " << j << endl;
+                res[i][j] = count++;
+            }
+       
+            cout << "Bottom to top" << endl;
+            // start i at last row and start j at first column, go until the row just below the initial row
+            for(i = n - startX - 2, j = startY; i >= startX + 1; i--) {
+                cout << "i " << i << " j " << j << endl;
+                res[i][j] = count++;
+            }
+            startY++;
+            startX++;
+        }            
+        return res;
+    }
+};    
