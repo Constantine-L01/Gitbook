d880776271369ee358673c2552b5ef734131db72
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-02-18 23:39:09 +0800

    Create 242. Valid Anagram(easy).cpp

---
+++ hashmap/242. Valid Anagram(easy).cpp
@@ -0,0 +1,34 @@
+Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+// array
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int record[26] = {0};
+        
+        for(int i = 0; i < s.size(); i++) {
+            record[s[i] - 'a']++;
+        }
+        
+        for(int i = 0; i < t.size(); i++) {
+            record[t[i] - 'a']--;
+        }
+        
+        for(int i = 0; i < 26; i++){
+            if(record[i] != 0){
+                return false;
+            }
+        }        
+        return true;
+    }
+};
