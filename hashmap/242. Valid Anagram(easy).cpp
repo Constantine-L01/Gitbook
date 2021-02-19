265e859b35325fbac04b6152389df3b6c1d349ce
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-02-19 10:21:46 +0800

    Update 242. Valid Anagram(easy).cpp

--- hashmap/242. Valid Anagram(easy).cpp
+++ hashmap/242. Valid Anagram(easy).cpp
@@ -32,3 +32,26 @@ public:
         return true;
     }
 };
+
+// unordered_map
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char, int> record;
+        
+        for(int i = 0; i < s.size(); i++) {
+            record[s[i] - 'a']++;
+        }
+        
+        for(int i = 0; i < t.size(); i++) {
+            record[t[i] - 'a']--;
+        }
+        
+        for(auto r: record){
+            if(r.second != 0){
+                return false;
+            }
+        }    
+        return true;
+    }
+};
