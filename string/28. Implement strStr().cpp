6c5c2ec58670ead58dfb8faa415339fb57b9bd51
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-02-22 20:29:45 +0800

    Update 28. Implement strStr().cpp

--- string/28. Implement strStr().cpp
+++ string/28. Implement strStr().cpp
@@ -19,18 +19,24 @@ public:
     void getNext(int* next, const string& s){
         int j = 0;
         next[0] = j;
+        // i should start at 1 because i represent the suffix
         for(int i = 1; i < s.size(); i++) {
+            // cannot compare when j == 0 and could not move the prefix more ahead since it is the first element
             while(j >= 1 && s[i] != s[j]) {
+                // by setting j - 1, infinite loop could be prevent as it will keep moving ahead.
                 j = next[j - 1];
             }
+            // if both equal then move j and i one step backward.
             if (s[i] == s[j]) {
                 j++;
             }
+            // the value at i will equal to the length of prefix 
             next[i] = j;
         }
     }
     
     int strStr(string haystack, string needle) {
+        // return if no needle
         if(needle.size() == 0){
             return 0;
         }
@@ -38,13 +44,19 @@ public:
         getNext(next, needle);
         int j = 0;
         for(int i = 0; i < haystack.size(); i++) {
+            // if not equal then move j to the element one step behind the prefix 
+            // keep moving back if still not equal
             while(j >= 1 && haystack[i] != needle[j]){
                 j = next[j-1];
             }
+            // move j one step forward if equal
             if(haystack[i] == needle[j]){
                 j++;
             }
-            if(j == (needle.size() )) {
+            // j == needle.size() because if last element match then j will increase by one
+            if(j == needle.size()) {
+                // need to + 1 because i start from 0 and needle.size() start from 1
+                // e.g. size of haystack is 10, i = 9 means found at last element, size of needle is 9, thus need to + 1 
                 return (i - needle.size() + 1);
             }
         }
