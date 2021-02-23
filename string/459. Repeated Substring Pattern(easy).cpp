34ee668469b0a1bb2aa5a9271635ad9e2f6c8ebf
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-02-23 18:04:43 +0800

    Update 459. Repeated Substring Pattern(easy).cpp

--- string/459. Repeated Substring Pattern(easy).cpp
+++ string/459. Repeated Substring Pattern(easy).cpp
@@ -19,3 +19,43 @@ public:
     }
 };
 
+// brute force
+class Solution {
+public:
+    bool isCopies(string s, string subStr){
+        if(s.size() % subStr.size() != 0){
+            return false;
+        }
+        
+        // will check through whole string to ensure there is no non-repeated area
+        for(int i = subStr.size(); i < s.size(); i += subStr.size()){
+            if(s.substr(i, subStr.size()) != subStr){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    bool repeatedSubstringPattern(string s) {
+        int sSize = s.size();
+        if(sSize == 1) {
+            return false;
+        }
+        
+        char first = s[0];
+        string subStr;
+        subStr+= first;
+        
+        // i <= sSize/2 because it cant have repeated substring if prefix is larger than half the string
+        for(int i = 1; i <= sSize/2; i++) {
+            if(first == s[i] && isCopies(s, subStr)) {
+                return true;
+            }
+            // if repeated substring require more char
+            else {
+                subStr+= s[i];
+            }
+        }        
+        return false;
+    }
+};
