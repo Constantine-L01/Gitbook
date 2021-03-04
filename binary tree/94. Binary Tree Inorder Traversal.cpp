e12c9059d183384ea54d20790ef927faf8cedb3a
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-03-05 06:59:39 +0800

    Update 94. Binary Tree Inorder Traversal.cpp

--- binary tree/94. Binary Tree Inorder Traversal.cpp
+++ binary tree/94. Binary Tree Inorder Traversal.cpp
@@ -64,3 +64,40 @@ public:
         return res;
     }
 };
+
+// universal style
+class Solution {
+public:
+   
+    vector<int> inorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st;
+        vector<int> res;
+        
+        if(root != nullptr){
+            st.push(root);
+        }
+        
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            
+            if(node != nullptr){
+                st.pop();
+                if(node->right) {
+                    st.push(node->right);
+                }
+                st.push(node);
+                st.push(nullptr);
+                if(node->left) {
+                    st.push(node->left);
+                }                
+            }
+            else {
+                st.pop();
+                node = st.top();
+                st.pop();
+                res.push_back(node->val);
+            }
+        }
+        return res;
+    }
+};
