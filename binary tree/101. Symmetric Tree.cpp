c7b769f9e8d3f7f58a8c353fedb0c5d78e9cd197
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-03-09 03:12:47 +0800

    Update 101. Symmetric Tree.cpp

--- binary tree/101. Symmetric Tree.cpp
+++ binary tree/101. Symmetric Tree.cpp
@@ -76,3 +76,38 @@ public:
         return true;        
     }
 };
+
+// Iterative with stack
+class Solution {
+public:    
+    bool isSymmetric(TreeNode* root) {
+        if(root == nullptr) {
+            return true;
+        }
+        stack<TreeNode*> st;
+        
+        if(root != nullptr){
+            st.push(root->left);
+            st.push(root->right);
+        }
+        
+        while(!st.empty()){
+            TreeNode* leftNode = st.top(); st.pop();
+            TreeNode* rightNode = st.top(); st.pop();
+            
+            if(!leftNode && !rightNode){
+                continue;
+            }
+            
+            if(!leftNode || !rightNode || (leftNode->val != rightNode->val)){
+                return false;
+            }
+            
+            st.push(leftNode->left);
+            st.push(rightNode->right);
+            st.push(leftNode->right);
+            st.push(rightNode->left);               
+        }
+        return true;        
+    }
+};
