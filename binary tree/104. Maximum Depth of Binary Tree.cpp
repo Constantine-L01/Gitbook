2438c8194f9efa00810a27960ed097911e80ddf6
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-03-10 05:33:54 +0800

    Update 104. Maximum Depth of Binary Tree.cpp

--- binary tree/104. Maximum Depth of Binary Tree.cpp
+++ binary tree/104. Maximum Depth of Binary Tree.cpp
@@ -31,3 +31,32 @@ public:
         return 1 + max(leftDepth, rightDepth);
     }
 };
+
+// iterative solution
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int depth = 0;
+        queue<TreeNode*> que;
+        
+        if(root != nullptr){
+            que.push(root);
+        }
+        
+        while(!que.empty()){
+            int size = que.size();
+            depth++;
+            for(int i = 0; i < size; i++){
+                TreeNode* node = que.front();
+                que.pop();
+                if(node->left){
+                    que.push(node->left);
+                }
+                if(node->right){
+                    que.push(node->right);
+                }
+            }
+        }        
+        return depth;
+    }
+};
