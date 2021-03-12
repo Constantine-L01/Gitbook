6f6519974900553d2b3bdb1ee2ac3f0e5c19e8be
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-03-12 12:04:57 +0800

    Update 77. Combinations.cpp

--- backtracking/77. Combinations.cpp
+++ backtracking/77. Combinations.cpp
@@ -39,3 +39,30 @@ public:
         return res;
     }
 };
+
+// optimized
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    
+    void backtracking(int n, int k, int startIndex){
+        if(path.size() == k){
+            res.push_back(path);
+            return;
+        }
+        
+        // need to +1 because need to include the first element e.g. n = 4, k = 4 but need to take in 1, then 2, then 3, then 4
+        for(int i = startIndex; i <= n - (k - path.size()) + 1; i++){
+            path.push_back(i);
+            backtracking(n, k, i + 1);
+            path.pop_back();
+        }
+        return;
+    }
+    
+    vector<vector<int>> combine(int n, int k) {     
+        backtracking(n, k , 1);
+        return res;
+    }
+};
