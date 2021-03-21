301fbe2ec7a25765278d66f4e8c18767dd450bdb
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-03-21 17:58:39 +0800

    Update 47. Permutations II.cpp

--- backtracking/47. Permutations II.cpp
+++ backtracking/47. Permutations II.cpp
@@ -42,3 +42,39 @@ public:
         return res;
     }
 };
+
+// another solution
+class Solution {
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    
+    void backtracking(vector<int>& nums, vector<bool>& used){
+        if(path.size() == nums.size()){
+            res.push_back(path);
+            return;
+        }
+        
+        for(int i = 0; i < nums.size(); i++){
+            // there will be a pair of path with the same ordering, thus we remove one path which take previous element first. 
+            if(i > 0 && nums[i] == nums[i - 1] && used[i - 1] == true){
+                continue;
+            }
+            
+            if(used[i] == false){
+                used[i] = true;
+                path.push_back(nums[i]);
+                backtracking(nums, used);
+                path.pop_back();
+                used[i] = false; 
+            }
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<bool> used(nums.size(), false);
+        sort(nums.begin(), nums.end());
+        backtracking(nums, used);
+        return res;
+    }
+};
