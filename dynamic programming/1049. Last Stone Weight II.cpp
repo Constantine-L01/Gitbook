4b616355d494be919eebcef97da83cfe81b59056
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-04-08 18:30:44 +0800

    Create 1049. Last Stone Weight II.cpp

---
+++ dynamic programming/1049. Last Stone Weight II.cpp
@@ -0,0 +1,42 @@
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+We are playing a game with the stones. On each turn, we choose any two stones and smash them together. Suppose the stones have weights x and y with x <= y. The result of this smash is:
+
+    If x == y, both stones are destroyed, and
+    If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+
+At the end of the game, there is at most one stone left.
+
+Return the smallest possible weight of the left stone. If there are no stones left, return 0. 
+
+Example 1:
+
+Input: stones = [2,7,4,1,8,1]
+Output: 1
+Explanation:
+We can combine 2 and 4 to get 2, so the array converts to [2,7,1,8,1] then,
+we can combine 7 and 8 to get 1, so the array converts to [2,1,1,1] then,
+we can combine 2 and 1 to get 1, so the array converts to [1,1,1] then,
+we can combine 1 and 1 to get 0, so the array converts to [1], then that's the optimal value.
+  
+class Solution {
+public:
+    int lastStoneWeightII(vector<int>& stones) {
+        int sum = 0;
+        for(int i = 0; i < stones.size(); i++){
+            sum += stones[i];
+        }
+        
+        int target = sum/2;
+        
+        vector<int> dp(target + 1, 0);
+        
+        for(int i = 0; i < stones.size(); i++){
+            for(int j = target; j >= stones[i]; j--){
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i]); 
+            }
+        }
+        
+        return (sum - dp[target]) - dp[target];  
+    }
+};
