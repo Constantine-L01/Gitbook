4aed6eb07ba8ffe14916c94136ffa7e8b25e0e07
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-04-25 21:21:39 +0800

    Update 392. Is Subsequence.cpp

--- dynamic programming/392. Is Subsequence.cpp
+++ dynamic programming/392. Is Subsequence.cpp
@@ -46,3 +46,21 @@ public:
         else return false;
     }
 };
+
+// reverse traversal, actually its not, need to think again
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        vector<vector<int>> dp(t.size() + 1, vector<int>(s.size() + 1, 0));
+        for (int i = 1; i <= t.size(); i++) {
+            for (int j = 1; j <= s.size(); j++) {
+                if (s[j - 1] == t[i - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = dp[i - 1][j];
+            }
+        }
+        if (dp[t.size()][s.size()] == s.size()) return true;
+        return false;
+    }
+};
+
+
