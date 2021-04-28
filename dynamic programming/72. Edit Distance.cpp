8f792cfd1f5a08d63bcb3109ea7ff96e7704492f
Andrew-L01 <77671482+Andrew-L01@users.noreply.github.com>
2021-04-28 11:14:22 +0800

    Create 72. Edit Distance.cpp

---
+++ dynamic programming/72. Edit Distance.cpp
@@ -0,0 +1,43 @@
+Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+
+You have the following three operations permitted on a word:
+
+    Insert a character
+    Delete a character
+    Replace a character 
+
+Example 1:
+
+Input: word1 = "horse", word2 = "ros"
+Output: 3
+Explanation: 
+horse -> rorse (replace 'h' with 'r')
+rorse -> rose (remove 'r')
+rose -> ros (remove 'e')
+  
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+        
+        for(int i = 0; i <= word1.size(); i++){
+            dp[i][0] = i;
+        }
+        for(int j = 0; j <= word2.size(); j++){
+            dp[0][j] = j;
+        }
+        
+        for(int i = 1; i <= word1.size(); i++){
+            for(int j = 1; j <= word2.size(); j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+                }
+            }
+        }
+        
+        return dp[word1.size()][word2.size()];
+    }
+};
