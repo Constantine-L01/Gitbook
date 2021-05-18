511addfc24e0bf815da6269eb47bd29241213d4b
Constantine-L01 <77671482+Constantine-L01@users.noreply.github.com>
2021-05-18 16:17:34 +0800

    Update 137. Single Number II.cpp

--- bit manipulation/137. Single Number II.cpp
+++ bit manipulation/137. Single Number II.cpp
@@ -26,3 +26,23 @@ public:
         return -1;
     }
 };
+
+// constant space, O(1)
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int count[32] = {0};
+        int result = 0;
+        
+        for(int i = 0; i < 32; i++){
+            for(int j = 0; j < nums.size(); j++){
+                if((nums[j] >> i) & 1){
+                    count[i]++;
+                }
+            }
+            result |= ((count[i] % 3) << i);
+        }
+        
+        return result;
+    }
+};
